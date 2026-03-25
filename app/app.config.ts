2ff21a8a661d36a4b0e9d4d2a00d786a405d622c
Fabrice Rosito <178981093+f-rosito@users.noreply.github.com>
2026-03-25 08:58:39 +0100

    feat(doc): Maj doc from 92f9a24 laravel-passkeys-api (#1)
    * feat(doc): Maj doc from 92f9a24 laravel-passkeys-api
    
    * Refactor code structure for improved readability and maintainability
    
    ---------
    
    Co-authored-by: f.rosito@thomyris.com <f.rosito@thomyris.com>

--- app/app.config.ts
+++ app/app.config.ts
@@ -12,10 +12,10 @@ export default defineAppConfig({
     }
   },
   seo: {
-    siteName: 'Nuxt Docs Template'
+    siteName: 'Laravel Passkey API'
   },
   header: {
-    title: '',
+    title: 'Laravel Passkey API',
     to: '/',
     logo: {
       alt: '',
@@ -26,45 +26,30 @@ export default defineAppConfig({
     colorMode: true,
     links: [{
       'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt-ui-templates/docs',
+      'to': 'https://github.com/xefi/laravel-passkeys-api',
       'target': '_blank',
       'aria-label': 'GitHub'
     }]
   },
   footer: {
-    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    credits: `Copyright © ${new Date().getFullYear()} Xefi`,
     colorMode: false,
     links: [{
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://go.nuxt.com/discord',
-      'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://go.nuxt.com/x',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
       'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
+      'to': 'https://github.com/xefi/laravel-passkeys-api',
       'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
+      'aria-label': 'Laravel Passkey API on GitHub'
     }]
   },
   toc: {
     title: 'Table of Contents',
     bottom: {
       title: 'Community',
-      edit: 'https://github.com/nuxt-ui-templates/docs/edit/main/content',
+      edit: 'https://github.com/xefi/laravel-passkeys-api-doc/edit/main/content',
       links: [{
         icon: 'i-lucide-star',
         label: 'Star on GitHub',
-        to: 'https://github.com/nuxt/ui',
-        target: '_blank'
-      }, {
-        icon: 'i-lucide-book-open',
-        label: 'Nuxt UI docs',
-        to: 'https://ui.nuxt.com/docs/getting-started/installation/nuxt',
+        to: 'https://github.com/xefi/laravel-passkeys-api',
         target: '_blank'
       }]
     }
